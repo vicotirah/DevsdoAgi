ac4a1d8f85ae54113c26580923705dae64036a53
Victória Rocha <victoria_rocha_26@hotmail.com>
2025-01-28 11:38:40 -0300

    Add files via upload

---
+++ DevsAgi/s2if2/Main.java
@@ -0,0 +1,15 @@
+package com.agibank.s2if2;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        scan.close();
+        if (i%2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
